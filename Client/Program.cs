86bc2df76c322fb11c30a055eae2c1c21be7411f
Renato Custodio <renatommc2002@gmail.com>
2023-10-01 23:31:32 +0100

    cenas

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,10 +6,13 @@ using Shared;
 
 public class Program
 {
+    public string name;
     public static void Main(string[] args)
     {
+        Program pm = new Program();
         string scriptPath = args[0];
         string configPath = args[1];
+        pm.name = args[2];
 
         ConfigurationManager configurationManager = new ConfigurationManager(configPath);
         ConnectionManager connectionManager = new ConnectionManager(configurationManager.TransactionManagers());
--- TransactionManager/Program.cs
+++ TransactionManager/Program.cs
@@ -5,20 +5,24 @@ using TransactionManager;
 
 public class Program
 {
+    public string nome;
     static void Main(string[] args)
     {
-
+        Program pm = new Program();
         if (args.Length < 1)
         {
             Console.WriteLine("ERROR: Arguments required");
             return;
         }
         
-        int port = int.Parse(args[0]);
+        string address = args[0];
+        int port = int.Parse(args[1]);
+        pm.nome = args[2];
+
         var server = new Server
         {
             Services = { TransactionManagerService.BindService(new TransactionService()) },
-            Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            Ports = { new ServerPort(address, port, ServerCredentials.Insecure) }
         };
         server.Start();
         Console.WriteLine($"Server listening at port {port}. Press any key to terminate");
