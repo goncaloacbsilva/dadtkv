75b2810b7f96e998e9a4c88c3f4796adaa808e11
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-10-02 19:13:30 +0100

    feat(TransactionManager): implement ServerExceptions Interceptor and logging system

--- TransactionManager/Program.cs
+++ TransactionManager/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Grpc.Core;
+using Grpc.Core.Interceptors;
+using Serilog;
 using Shared;
 using TransactionManager;
 
@@ -8,6 +10,7 @@ public class Program
 {
     static void Main(string[] args)
     {
+        var logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.Console().CreateLogger();
 
         if (args.Length < 3)
         {
@@ -22,15 +25,17 @@ public class Program
         string address = args[2];
         int port = int.Parse(args[3]);
 
-        var configurationManager = new ConfigurationManager(configPath);
+        var configurationManager = new ConfigurationManager(configPath, identifier);
         
         var server = new Server
         {
-            Services = { TransactionManagerService.BindService(new TransactionService(configurationManager, identifier)) },
+            Services = { TransactionManagerService.BindService(new TransactionService(configurationManager, identifier)).Intercept(new ServerExceptionsInterceptor(logger)) },
             Ports = { new ServerPort(address, port, ServerCredentials.Insecure) }
         };
+        
+        
         server.Start();
-        Console.WriteLine($"Server listening at port {port}. Press any key to terminate");
+        logger.Information($"Server listening at port {port}. Press any key to terminate");
         Console.ReadKey();
     }   
 }
