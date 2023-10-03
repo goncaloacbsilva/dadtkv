402c57e9f317871d583650a8127eae4378704a55
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-10-03 23:38:32 +0100

    feat(LeaseManager): implement IsLeader method

--- LeaseManager/LeaseService.cs
+++ LeaseManager/LeaseService.cs
@@ -17,7 +17,7 @@ public class LeaseService : LeaseManagerService.LeaseManagerServiceBase
         _configurationManager = configurationManager;
         _logManager = logManager;
 
-        _configurationManager.NextSlotEvent += (sender, args) => { TestFunction(); };
+        _configurationManager.NextSlotEvent += (sender, args) => { RunPaxos(); };
     }
 
     public override Task<LeaseRequestResponse> Request(LeaseRequest request, ServerCallContext context)
@@ -27,8 +27,38 @@ public class LeaseService : LeaseManagerService.LeaseManagerServiceBase
         return base.Request(request, context); 
     }
 
+    private bool CurrentIsLeader()
+    {
+        _logManager.Logger.Debug("[Paxos]: Am I a leader?");
+        try
+        {
+            var currentSuspected = _configurationManager.CurrentSuspects.Select(suspect => suspect.suspected).ToList();
+            var leaseServers = _configurationManager.Servers
+                .Where(server =>
+                    (server.type == ServerType.Lease) &&
+                    String.CompareOrdinal(server.identifier, _configurationManager.Identifier) < 0)
+                .Select(server => server.identifier).ToList();
 
-    public async void TestFunction() {
-        _logManager.Logger.Debug("Test function trigered!!");
+            _logManager.Logger.Debug("[Paxos]: Servers that I'm watching: {0}", leaseServers);
+            _logManager.Logger.Debug("[Paxos]: Suspected servers: {0}", currentSuspected);
+            
+            return leaseServers.All(currentSuspected.Contains);
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    public async void RunPaxos() {
+        // Check if leader is alive with suspects
+        if (CurrentIsLeader())
+        {
+            _logManager.Logger.Information("[Paxos]: Im a leader, starting epoch {0}", _configurationManager.CurrentEpoch);
+        }
+        else
+        {
+            _logManager.Logger.Debug("[Paxos]: Im not a leader");
+        }
     }
 }
